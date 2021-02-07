1521d7d2ff175638212013aa3dd503a97f8de151
Daniel Santa Rosa <iel_182@hotmail.com>
2021-02-07 12:01:43 -0300

    first commit

---
+++ src/LearnAzureDevOps.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LearnAzureDevOps.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
